dbec65af3a01dcf7dc60876a4c57ad6510d44390
= <ajfisher.td@gmail.com>
2011-11-27 11:58:01 +1100

    Initial commit of project

---
+++ djarduino_mac_maker/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djarduino_mac_maker.views.home', name='home'),
+    # url(r'^djarduino_mac_maker/', include('djarduino_mac_maker.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
